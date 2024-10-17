c98cc2a6d5480973bbd9613b7f26aa32b92862e2
fmzaar <fmzaar@gmail.com>
2024-10-17 13:35:32 -0300

    Creación del proyecto y configuración H2

---
+++ src/test/java/francosuarez/parcial/MutantsApplicationTests.java
@@ -0,0 +1,13 @@
+package francosuarez.parcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
